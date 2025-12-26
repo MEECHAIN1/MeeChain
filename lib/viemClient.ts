9fc7c9f2fe3b6fc2099191a9f8dc6fe40ce358e1
ธณวัฒน์ ประวันตา <pouaun2499@gmail.com>
2025-12-26 21:18:20 +0700

    Updated viemClient.ts

--- lib/viemClient.ts
+++ lib/viemClient.ts
@@ -1,7 +1,7 @@
 import { createPublicClient, http, fallback } from 'viem';
 import { localhost } from 'viem/chains';
 
-const RPC_URL = process.env.VITE_RPC_URL || 'https://shape-mainnet.g.alchemy.com/v2/J1HfoMSvISZdnANVlkTA6';
+const RPC_URL = process.env.VITE_RPC_URL || "https://shape-mainnet.g.alchemy.com/v2/J1HfoMSvISZdnANVlkTA6";
 
 export const meechain = {
   ...localhost,
