219b58a020e7b681166e47cf3af91953457c4808
ธณวัฒน์ ประวันตา <pouaun2499@gmail.com>
2025-12-29 18:51:02 +0700

    Update chains.ts

--- lib/constants/chains.ts
+++ lib/constants/chains.ts
@@ -3,18 +3,18 @@ import { Chain } from 'viem';
 
 // MeeChain Configuration updated with provided QuikNode RPC
 export const meechain: Chain = {
-  id: 137, // Mapping to Polygon Mainnet as per QuikNode URL
-  name: 'MeeChain Ritual (Polygon)',
+  id: 56, // Mapping to Polygon Mainnet as per QuikNode URL
+  name: 'MeeChain Ritual (bsc)',
   nativeCurrency: {
     decimals: 18,
-    name: 'POL',
-    symbol: 'POL',
+    name: 'meechain',
+    symbol: 'MCB',
   },
   rpcUrls: {
-    default: { http: ['https://dimensional-newest-film.matic.quiknode.pro/8296e7105d470d5d73b51b19556495493c8f1033'] },
-    public: { http: ['https://dimensional-newest-film.matic.quiknode.pro/8296e7105d470d5d73b51b19556495493c8f1033'] },
+    default: { http: ['https://dimensional-newest-film.bsc.quiknode.pro/8296e7105d470d5d73b51b19556495493c8f1033'] },
+    public: { http: ['https://dimensional-newest-film.bsc.quiknode.pro/8296e7105d470d5d73b51b19556495493c8f1033'] },
   },
   blockExplorers: {
-    default: { name: 'PolygonScan', url: 'https://polygonscan.com' },
+    default: { name: 'bscscan', url: 'https://bscscan.com' },
   },
 };
