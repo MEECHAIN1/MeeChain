21eec304afbd176014d2eae81661ae0b6c87c454
ธณวัฒน์ ประวันตา <pouaun2499@gmail.com>
2025-12-29 13:41:21 +0700

    Updated viemClient.ts

--- lib/viemClient.ts
+++ lib/viemClient.ts
@@ -2,7 +2,7 @@
 import { createPublicClient, http, fallback } from 'viem';
 import { meechain } from './constants/chains';
 
-const RPC_URL = "https://dimensional-newest-film.matic.quiknode.pro/8296e7105d470d5d73b51b19556495493c8f1033";
+const RPC_URL = "https://dimensional-newest-film.bsc.quiknode.pro/8296e7105d470d5d73b51b19556495493c8f1033";
 
 export { meechain };
 
