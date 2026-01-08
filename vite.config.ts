37c60eca4c78402539f540fa4265d03b34e12be0
ธณวัฒน์ ประวันตา <pouaun2499@gmail.com>
2026-01-08 19:18:33 +0700

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,8 @@
-
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
- export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '.', '');
-  return {
-    server: {
-      port: 3000,
-      host: '0.0.0.0',
-    },
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
-      },
-      dedupe: ['ox'], // 🔮 Deduplicate ox across nested dependencies
-    },
-    build: {
-      sourcemap: true,
-       rollupOptions: {
-         output: {
-          }
-        }
-      }
-    }
-  });
+export default defineConfig({
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+});
