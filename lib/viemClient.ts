590557d4016c4406dcb07f46d614c9d442b64c38
ธณวัฒน์ ประวันตา <pouaun2499@gmail.com>
2026-01-08 11:21:01 +0700

    Updated viemClient.ts

--- lib/viemClient.ts
+++ lib/viemClient.ts
@@ -1,6 +1,6 @@
 
 import { createPublicClient, http } from 'viem';
-import { meechain } from './lib/constants/chains';
+import { meechain } from "./constants/chains";
 
 // Using standard http transport for maximum compatibility and stability
 const PRIMARY_RPC = "https://bsc-dataseed.binance.org/";
