bba1648d23b24ebfcc5a416d7c215173e61934ef
ธณวัฒน์ ประวันตา <pouaun2499@gmail.com>
2026-01-08 12:49:41 +0700

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -24,8 +24,8 @@ import react from '@vitejs/plugin-react'; // Ensure this import is correct
     build: {
       sourcemap: true,
       rollupOptions: {
-      output: {
-          },
+     // output: {
+          }, /*
         }
       }
     }
