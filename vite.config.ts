a97cab5918874bfd35f2376a807823c183a4bd8a
ธณวัฒน์ ประวันตา <pouaun2499@gmail.com>
2026-01-08 19:18:07 +0700

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import react from '@vitejs/plugin-react'; // Ensure this import is correct
+
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
