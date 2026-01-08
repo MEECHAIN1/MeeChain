2ef444199afa6e854160ac7a2812284ea1c08ffe
ธณวัฒน์ ประวันตา <pouaun2499@gmail.com>
2026-01-08 13:03:21 +0700

    Updated index.tsx

--- index.tsx
+++ index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { Buffer } from 'buffer';
+import ErrorBoundary from './components/ErrorBoundary'; // ตรวจสอบ Path ให้ถูกต้อง
 window.global = window;
 window.Buffer = Buffer;
 
