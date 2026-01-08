91acde35fb66f15ed66295d6717d48b8cc6d406b
ธณวัฒน์ ประวันตา <pouaun2499@gmail.com>
2026-01-08 12:42:40 +0700

    Updated wagmi.ts

--- lib/wagmi.ts
+++ lib/wagmi.ts
@@ -5,7 +5,7 @@ import { injected, walletConnect } from 'wagmi/connectors';
 const metadata = {
   name: 'MeeBot Chain',
   description: 'MeeBot Ecosystem Ritual Portal',
-  url: typeof window !== 'undefined' ? window.location.origin : 'https://meebot.meechain.io',
+  url: typeof window !== 'undefined' ? window.location.origin : 'https://127.0.0.1',
   icons: ['https://avatars.githubusercontent.com/u/37784886'],
 };
 
