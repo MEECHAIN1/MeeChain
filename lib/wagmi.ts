9297fd0ccbe5670bc05087d6970908a23337ba3c
ธณวัฒน์ ประวันตา <pouaun2499@gmail.com>
2026-01-08 12:45:13 +0700

    Updated wagmi.ts

--- lib/wagmi.ts
+++ lib/wagmi.ts
@@ -6,7 +6,6 @@ const metadata = {
   name: 'MeeBot Chain',
   description: 'MeeBot Ecosystem Ritual Portal',
   url: typeof window !== 'undefined' ? window.location.origin : ['https://127.0.0.1:8545'],
-  icons: ['https://avatars.githubusercontent.com/u/37784886'],
 };
 
 export const config = createConfig({
