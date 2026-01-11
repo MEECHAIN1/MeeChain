578bbee1c475ad173a2897bef446295380242a5c
ธณวัฒน์ ประวันตา <pouaun2499@gmail.com>
2026-01-11 19:57:26 +0700

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => {
     },
     plugins: [react()],
     define: {
-     'import.meta.env.VITE_API_KEY': JSON.stringify(env.VITE_API_KEY),
+    // 'import.meta.env.VITE_API_KEY': JSON.stringify(env.VITE_API_KEY),
     },
     resolve: {
       alias: {
