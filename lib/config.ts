e451602c5f9eb7160e0d6b847415d8b9692833ed
ธณวัฒน์ ประวันตา <pouaun2499@gmail.com>
2026-01-14 10:19:49 +0700

    Updated config.ts

--- lib/config.ts
+++ lib/config.ts
@@ -13,7 +13,7 @@ export const CONFIG = {
   },
   TOKENS: {
     CLIENT_ROLLBAR: '1d6379b7fbb5403b8cf07e4a9c4889e2',
-    PROJECT_ID_WAGMI: '663c25b58c5d2037993b7b5d5d35f3aa'
+    PROJECT_ID_WAGMI: 'b0d81328f8ab0541fdede7db9ff25cb1'
   },
   STORAGE_KEYS: {
     BOTS: 'meebot_collective_data',
