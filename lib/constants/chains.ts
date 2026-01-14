46640eadc95ffc761b8b03b71354ab7a0a8194a5
ธณวัฒน์ ประวันตา <pouaun2499@gmail.com>
2026-01-14 12:27:31 +0700

    Updated chains.ts

--- lib/constants/chains.ts
+++ lib/constants/chains.ts
@@ -4,7 +4,7 @@ import { Chain } from 'viem';
 // MeeChain Configuration updated to use BNB Smart Chain (BSC) as the primary protocol
 export const meechain: Chain = {
   id: 13390, // Binance Smart Chain Mainnet
-  name: 'MeeChain Ritual (BSC)',
+  name: 'MeeChain Ritual (MCB)',
   nativeCurrency: {
     decimals: 18,
     name: 'MeeChain',
